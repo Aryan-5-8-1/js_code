7e1bb5b6dc3cbef797e76c8a0c79047cdca7ea13
Aryan-5-8-1 <114466763+Aryan-5-8-1@users.noreply.github.com>
2023-09-28 16:36:41 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,36 @@
+let score="33afg"
+
+// console.log(typeof(score))
+/*
+here we use type of because sometimes as we work on backend we dont know 
+the type of value.
+*/
+
+let valueInNumber=Number(score)
+/*
+it is converted to number means score basically is string and than by applying this we convert this 
+into number. But it is not necessary ki vo convert ho hi jaaye.
+
+*/
+ // console.log(typeof(valueInNumber))
+// it again shows strings.
+
+// "33" => 33
+// "33sjh" =>NaN
+// T => 1; F =>0
+ 
+let isLoggedIn=""
+
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+ // console.log(booleanIsLoggedIn);
+ // 1 -> t;0 -> f
+ // "" ->false
+ // " atyab" -> true
+
+let someNumber=33
+let stringNumber=String(someNumber)
+console.log(stringNumber);
+console.log(typeof(stringNumber)); // number -> string
+
+
+
