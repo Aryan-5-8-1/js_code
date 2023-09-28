b62a557b7e73842545a1f5843f11c633cb014ad4
Aryan-5-8-1 <114466763+Aryan-5-8-1@users.noreply.github.com>
2023-09-28 14:42:55 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,28 @@
+const accountId = 12456
+let accountEmail= "aryan52145@google.com"
+var accountPassword="12345"
+accountCity="Jaipur"
+let accountState;
+
+/*
+accountcity== me bina kuch likhe hogya so js main 
+bina kuch use kiye kuch bhi chal jata hain.
+
+accountState=if we dont assign anything js understand it undefined.
+
+ const,let,var all are keywords of js.
+  initially java dont work on scopes {} =.
+
+  PREFER NOT TO USE VAR BECAUSE 
+  of issue in block scope and functional scope
+  so Thts why in modern time we only use let and const.
+  */
+
+ //accountId =2; when i change this ,yeh hoga kinahi hoga yeh analyze karega node js
+
+ accountEmail="satyam50014@gmail.com"
+ accountPassword="56489"
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+// console.table gives us all the data in a tabular form ,if we change some data or not it doesnt matter.
+
