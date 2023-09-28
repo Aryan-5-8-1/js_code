10d16770bc601e21533773f15916f36f177abfa9
Aryan-5-8-1 <114466763+Aryan-5-8-1@users.noreply.github.com>
2023-09-28 15:41:56 +0000

    data types

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all JS code as newer version.
+
+ // alert(3+3) // we are using nodejs not browser.
+/*
+here we cannot use alert directly with same syntax,
+it have differnt syntax in nodejs.
+*/
+
+let name="aryan"
+let age="18"
+let isLoggedIn=false
+ 
+// PRIMITIVE DATATYPES
+// number => 2^53
+// bigint=very large number
+// string=""
+// boolean=t/f
+// null =>standalone value or representation of empty value.
+
+// undefined => 
+// symbol => used in react to uniquely identify
+
+// OBJECTS
+console.log(typeof(2))
+console.log(typeof(null)) // it comes object means null is an object
