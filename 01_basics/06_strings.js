a8a7f1c2b9816c4ade1a84edd358f2f3dae50cab
Aryan-5-8-1 <114466763+Aryan-5-8-1@users.noreply.github.com>
2023-10-01 05:31:23 +0000

    math and num

--- 01_basics/06_strings.js
+++ 01_basics/06_strings.js
@@ -3,11 +3,11 @@ const repoCount=50;
 
 // console.log(name+repoCount); // it is outdated to use
 // we use beckticks
-console.log(`hello my name is ${name} and my repo count is ${repoCount}`); 
+// console.log(`hello my name is ${name} and my repo count is ${repoCount}`); 
 // Above  is bektics
 
 // we also decleare string by using new keyword.
-const gameName= new String('Aryan')
+const gameName= new String('Aryan_tiwari')
 
 /*
 SO DONT BE CONFUSE THAT IT IS AN ARRAY,IT IS KEY VALUE PAIRS
@@ -29,8 +29,22 @@ AT 0-> A IS PRESENT LIKE THAT
 "n"
 */
 
-console.log(gameName[0]) // op: A BECAUSE KEY VALUE PAIRS.
-console.log(gameName.length);
-console.log(gameName.toUpperCase());
-console.log(gameName.__proto__);
-console.log(gameName.charAt(2));
+// console.log(gameName[0]) // op: A BECAUSE KEY VALUE PAIRS.
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.__proto__);
+// console.log(gameName.charAt(2));
+
+// console.log(gameName.substring(0,5)); // in this we cant use negeative values.
+// console.log(gameName.slice(-10,4));   // it takes negeative values.
+
+const A=new String(" aryan ")
+// console.log(A);
+// console.log(A.trim());
+
+const url="https://aryan.com/aryan%20tiwari"
+console.log(url.replace('%20','-'));
+
+
+
+
---
+++ 01_basics/07_num&maths.js
@@ -0,0 +1,49 @@
+const score=400 // here js automatically define that is the number.
+console.log(score);
+
+// But ewe define by own that it is number.
+const balance=new Number(500)
+console.log(balance);
+
+// number converted to string
+console.log(balance.toString);
+
+console.log(balance.toFixed(2));
+// Number of digits after the decimal point. Must be in the range 0 - 20, inclusive.
+// Returns a string representing
+//  a number in fixed-point notation.
+
+console.log(balance.toPrecision(2));
+
+/*
+Returns a string containing a number represented 
+either in exponential or fixed-point notation with a specified number of digits.
+
+@param precision — Number of significant digits. Must be in the range 1 - 21, inclusive.
+*/
+
+const hundreds=10000000
+console.log(hundreds.toLocaleString());
+console.log(hundreds.toLocaleString('en-IN'));
+
+// ++++++++++++++++++++(MATHS)+++++++++++++++++++
+// console.log(Math);         // math ek object hain
+// console.log(Math).abs(-4);  // -ve ko +ve kardega
+// console.log(Math.round(4.6)); // round off
+// console.log(Math.ceil(4.5)); // jara bhi 4 se zada hua 5 return kardega
+// console.log(Math.floor(4.9)); // 4 ayega neeche wali values legaa
+
+// +++++(Math.random())+++++Important
+// in this always values comes btw 0 and 1.
+// when we need random values betweeen 2 numbers we use it.
+console.log(Math.random());
+// if we want random values between 1 to 10
+console.log(Math.random()*10);// yeh dedegaa but isme 0 bhi aasakta hain
+// thats why now we add 1 also
+console.log((Math.random()*10)+1);
+// Q: find the random values between 10 and 20
+
+const min=10;
+const max=20;
+ console.log(Math.floor((Math.random()*(max-min + 1) + min)));
+ // so above part is very important:
